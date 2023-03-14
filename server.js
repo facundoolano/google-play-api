40fb7eaedd6c9f0b217207eb0b8284e61679dcfa
Jason Vu <jvu@scu.edu>
2023-03-14 16:56:15 -0700

    Accomplished:
    created buttons for making annotations for each app id, saves it using local storage and can be downloaded as csv file
    Need to Do:
    
    need to write backend endpoint to save annotations in the /annotations directory
    
    recursively frontload all app information without taxing api limits to interface

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = require('./lib');
+const express = require("express");
+const router = require("./lib");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/', router);
+app.use("/api/", router);
 
-app.get('/', function (req, res) {
-  res.redirect('/api');
+app.get("/", function (req, res) {
+  res.redirect("/api");
 });
 
 app.listen(port, function () {
-  console.log('Server started on port', port);
+  console.log("Server started on port", port);
 });
--- server_new.js
+++ server_new.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const router = require('./lib');
-const path = require('path');
+const express = require("express");
+const router = require("./lib");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/', router);
-app.use(express.static(path.join(__dirname, '.')));
+app.use("/api/", router);
+app.use(express.static(path.join(__dirname, ".")));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html")); 
 });
 
 app.listen(port, function () {
-  console.log('Server started on port', port);
+  console.log("Server started on port", port);
 });
