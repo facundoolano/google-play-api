8d7990b364a59a782d6414a2a39fbafef4cc0972
Jason Vu <jvu@scu.edu>
2023-04-12 17:59:27 -0700

    clean up repo

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2017 LambdaClass
+Copyright (c) 2023 Jason Vu
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- index.js
+++ index.js
@@ -1,118 +1,120 @@
 const { app, BrowserWindow, ipcMain, session } = require("electron");
 const gplay = require("google-play-scraper"); // https://www.npmjs.com/package/google-play-scraper
-  
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      // Set the Content Security Policy for the renderer process
-      // Only allow content to be loaded from the same origin
-      // Disable inline script and eval functions
-      // Enable remote module (use with caution)
-      // See https://www.electronjs.org/docs/tutorial/security#6-enable-context-isolation-for-remote-content
-      // for more information about remote module and context isolation 
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-      contentSecurityPolicy: "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'"
-    }
-  });
+	const mainWindow = new BrowserWindow({
+		webPreferences: {
+			// Set the Content Security Policy for the renderer process
+			// Only allow content to be loaded from the same origin
+			// Disable inline script and eval functions
+			// Enable remote module (use with caution)
+			// See https://www.electronjs.org/docs/tutorial/security#6-enable-context-isolation-for-remote-content
+			// for more information about remote module and context isolation 
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true,
+			contentSecurityPolicy: "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'"
+		}
+	});
 
-  mainWindow.loadFile("index.html");
+	mainWindow.loadFile("index.html");
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-  createWindow();
+	createWindow();
 
-  app.on("ready", () => {
-    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-      const newHeaders = Object.assign({}, details.responseHeaders, {
-        'Content-Security-Policy': ["default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'"],
-      });
-      callback({ responseHeaders: newHeaders });
-    });
-  });    
+	app.on("ready", () => {
+		session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+			const newHeaders = Object.assign({}, details.responseHeaders, {
+				'Content-Security-Policy': ["default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'"],
+			});
+			callback({
+				responseHeaders: newHeaders
+			});
+		});
+	});
 
-  app.on("activate", function() {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	app.on("activate", function() {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
 app.on("window-all-closed", function() {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
- 
+
 /* App Search */
 ipcMain.on('search', async (event, searchTerm) => {
-  try {
-    const results = await searchTermMore(searchTerm);
-    event.sender.send('search-results', JSON.stringify(results), searchTerm);
-  } catch (err) {
-    event.sender.send('search-error', err);
-  }
+	try {
+		const results = await searchTermMore(searchTerm);
+		event.sender.send('search-results', JSON.stringify(results), searchTerm);
+	} catch (err) {
+		event.sender.send('search-error', err);
+	}
 });
 
 async function searchTermMore(term, arr = {}) {
-  if (!term) {
-  console.error("Error: search term is undefined");
-  return;
-  }
-  
-  let total = 0;
-  let i = 0;
-  let count = 0;
-  
-  console.log("searchTermMore called with term:", term);
-  
-  try {
-  const apps = await gplay.search({
-  term: term,
-  num: 20,
-  });
-   
-  total = apps.length;
-  
-  console.log(`Found ${total} apps for term: ${term}`);
-  
-  if (apps.length === 0) {
-    console.log(`No apps found for term: ${term}`);
-    return Object.values(arr);
-  }
-  
-  await Promise.all(
-    apps.map(async (app) => {
-      try {
-        const apps2 = await gplay.search({
-          term: app.title,
-          num: 100,
-        });
-  
-        console.log(`Found ${apps2.length} apps for subterm: ${app.title}`);
-  
-        for (let app of apps2) {
-          if (!(app.appId in arr)) {
-            arr[app.appId] = app;
-            i++;
-          }
-        }
-      } catch (err) {
-        console.error(`Error occurred during subterm search for ${app.title}:`, err);
-      }
-      count++;
-    })
-  );
-  
-  if (count < total) {
-    console.log(`Not all apps for term: ${term} have been processed, calling searchTermMore again`);
-    return await searchTermMore(term, arr);
-  } else {
-    console.log("searchTermMore returning arr:", Object.values(arr));
-    return Object.values(arr);
-  } 
-  } catch (err) {
-  console.error("Error occurred during search:", err);
-  throw err;
-  }
+	if (!term) {
+		console.error("Error: search term is undefined");
+		return;
+	}
+
+	let total = 0;
+	let i = 0;
+	let count = 0;
+
+	console.log("searchTermMore called with term:", term);
+
+	try {
+		const apps = await gplay.search({
+			term: term,
+			num: 20,
+		});
+
+		total = apps.length;
+
+		console.log(`Found ${total} apps for term: ${term}`);
+
+		if (apps.length === 0) {
+			console.log(`No apps found for term: ${term}`);
+			return Object.values(arr);
+		}
+
+		await Promise.all(
+			apps.map(async (app) => {
+				try {
+					const apps2 = await gplay.search({
+						term: app.title,
+						num: 100,
+					});
+
+					console.log(`Found ${apps2.length} apps for subterm: ${app.title}`);
+
+					for (let app of apps2) {
+						if (!(app.appId in arr)) {
+							arr[app.appId] = app;
+							i++;
+						}
+					}
+				} catch (err) {
+					console.error(`Error occurred during subterm search for ${app.title}:`, err);
+				}
+				count++;
+			})
+		);
+
+		if (count < total) {
+			console.log(`Not all apps for term: ${term} have been processed, calling searchTermMore again`);
+			return await searchTermMore(term, arr);
+		} else {
+			console.log("searchTermMore returning arr:", Object.values(arr));
+			return Object.values(arr);
+		}
+	} catch (err) {
+		console.error("Error occurred during search:", err);
+		throw err;
+	}
 }
